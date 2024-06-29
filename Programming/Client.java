4d0619e8b525e14886212c422b5991c96d313f3e
Kaihan <120945229+NoyanKaihan@users.noreply.github.com>
2024-06-29 16:52:10 +0200

    Update Client.java

--- Programming/Client.java
+++ Programming/Client.java
@@ -1,1 +1,21 @@
+import java.util.Scanner;
+import java.net.*;
+import java.io.*;
 
+public class Client{
+      public static void main (String [] args){
+          Scanner input = new Scanner (System.in);
+          try{
+              Socket socket = new Socket ("localhost",1924);
+              InputStream inputStream  = socket.getInputStream();
+              OutputStream outputStream = socket.getOutputStream();
+              ObjectInputStream objectInputStream = new ObjectInputStream (inputStream);
+              ObjectOutputStream objectOutputStream = new ObjectOutputStream (outputStream);
+              while (true){
+                System.out.println ("Enter text to echo =");
+                String messageToEcho = input.nextLine ();
+                objectOutputStream.wirteObject (messageToEcho);
+              }
+          }
+      }
+              
